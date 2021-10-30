b7256f6275df758ed6a2dd74f318b9afff7742d1
Klara <kveljko1@binghamton.edu>
2021-10-29 21:36:41 -0400

    Hi

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 
 def main():
     pygame.init()
-    team = {"lead": "Madeline Scotti", "backend": "?", "frontend": "?"}
+    team = {"lead": "Madeline Scotti", "backend": "?", "frontend": Klara Veljkovic"?"}
     print("Software Lead is:", team["lead"])
     print("Backend is:", team["backend"])
     print("Frontend is:" , team["frontend"])
