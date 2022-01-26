9d663b37be8c681a521578aca23fe3185c0238d3
Steven A Moore <stevenallenmoore@gmail.com>
2022-01-26 22:58:38 +0000

    nothing

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ def main():
     pygame.init()
     game = Controller.Controller()
     game.mainLoop()
-	
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 2 LINES OF CODE ######
+
 main()
